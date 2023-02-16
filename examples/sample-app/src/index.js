20c407fed2833e8bf23f7189652a75702709fc43
dyu <dyu@bizflow.com>
2023-02-16 14:31:39 +0900

    add example blocks for FEEL

--- examples/sample-app/src/index.js
+++ examples/sample-app/src/index.js
@@ -5,16 +5,14 @@
  */
 
 import * as Blockly from 'blockly';
-import {blocks} from './blocks/text';
-import {generator} from './generators/javascript';
-import {javascriptGenerator} from 'blockly/javascript';
+import {blocks} from './blocks/json';
+import {jsonGenerator} from './generators/json';
 import {save, load} from './serialization';
 import {toolbox} from './toolbox';
 import './index.css';
 
 // Register the blocks and generator with Blockly
 Blockly.common.defineBlocks(blocks);
-Object.assign(javascriptGenerator, generator);
 
 // Set up UI elements and inject Blockly
 const codeDiv = document.getElementById('generatedCode').firstChild;
@@ -26,12 +24,8 @@ const ws = Blockly.inject(blocklyDiv, {toolbox});
 // generated code from the workspace, and evals the code.
 // In a real application, you probably shouldn't use `eval`.
 const runCode = () => {
-  const code = javascriptGenerator.workspaceToCode(ws);
+  const code = jsonGenerator.workspaceToCode(ws);
   codeDiv.innerText = code;
-
-  outputDiv.innerHTML = '';
-
-  eval(code);
 };
 
 // Load the initial state from storage and run the code.
